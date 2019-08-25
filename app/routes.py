75f050f9126795e00012ce66965d34d8c9937ddc
jefflig <jefflpersonal@gmail.com>
2019-08-25 14:12:43 -0500

    fancier routes and login page, all the way through #3 in tutorial

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from app.forms import LoginForm
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
@@ -11,6 +11,9 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
-
 
