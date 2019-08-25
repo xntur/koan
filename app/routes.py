0e1b46d5220ffb75e3544abec98c1cafffe2308b
jefflig <jefflpersonal@gmail.com>
2019-08-25 13:54:07 -0500

    simple login page that doesn't do anything yet + topbar

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
+
