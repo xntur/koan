ddff9da6d911dee27d0325bcc6eede185159d275
jefflig <jefflpersonal@gmail.com>
2019-08-27 20:57:28 -0500

    Create the db tables if they aren't populated yet

--- app/db.py
+++ app/db.py
@@ -17,9 +17,40 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+
+# Everything below just sets up the databases.        
+create_sql_teams_table = """
+  CREATE TABLE IF NOT EXISTS dependencies (
+    id integer PRIMARY KEY,
+    name text NOT NULL,
+    points integer
+  );"""
+create_sql_answered_table = """
+  CREATE TABLE IF NOT EXISTS answered (
+    team_id integer NOT NULL,
+    question_id integer NOT NULL,
+    PRIMARY KEY (team_id, question_id)
+  );"""
+create_sql_questions_table = """
+  CREATE TABLE IF NOT EXISTS questions (
+    id integer PRIMARY KEY,
+    name text NOT NULL,
+    question text NOT NULL,
+    count integer NOT NULL,
+    points integer NOT NULL
+  );"""
+create_sql_dependencies_table = """
+  CREATE TABLE IF NOT EXISTS questions (
+    solve_first integer NOT NULL,
+    solve_next integer NOT NULL,
+    PRIMARY KEY (solve_first, solve_next)
+  );"""
 @app.before_first_request
 def maybe_create_tables():
-    if len(get_db().execute('SELECT * FROM sqlite_master WHERE type="table"').fetchall()) == 0:
-        
-
-    
+    conn = get_db()
+    c = conn.cursor()
+    c.execute(create_sql_teams_table)
+    c.execute(create_sql_dependencies_table)
+    c.execute(create_sql_questions_table)
+    c.execute(create_sql_answered_table)
+    conn.commit()
