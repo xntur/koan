4593c18bba256812c3e59c3ac17acc27464c0604
jefflig <jefflpersonal@gmail.com>
2019-09-21 19:14:18 -0500

    more questions

---
+++ solutions/solutions.py
@@ -0,0 +1,48 @@
+import operator
+
+def last10(n):
+    return n % 10000000000
+
+def fib(n):
+    before = 1
+    curr = 1
+    n -= 2
+    while n > 0:
+        tmp = curr
+        curr += before
+        before = tmp
+        n -= 1
+    return last10(curr)
+
+def numTrees(n):
+    if n <= 1:
+        return 1
+    tmp = list(range(0, n+1))
+    tmp[0] = 1
+    for i in range(3, len(tmp)):
+        total = 0
+        right = i - 1
+        left = 0
+        while left <= right:
+            total += tmp[left] * tmp[right - left]
+            left += 1
+        tmp[i] = total
+    return last10(tmp[-1])
+
+def charsbycount(chars):
+    ans = {}
+    for char in chars:
+        if char not in ans:
+            ans[char] = 0
+        ans[char] += 1
+    sorted_x = sorted(ans.items(), key=operator.itemgetter(1))
+    out = [x[0] for x in sorted_x]
+    return out 
+
+print("fibonacci: " + str(fib(70000)))
+print("bsts: " + str(numTrees(100)))
+
+teststring = "nxmbbzmmailmmanrbjwfarvnzviikhhiabpsjtqrzbrqizirziddmtjwvkavofaggvjmflyhhhqsfirptznvxxbdmrbumrqfibvndimlubrjnzmfrrmngrbahjvrskfxxnnnrdvhvzkiffsxnrdrnirdffmambxehvfttzlbcdifiljrgmimsxhokzsxtlandrfsrmvvaphtwinnbjwifjdsjjgvxbkcyglkkjzflxwzdndjibzljfkwfwufktzjrjnttlupnjnzffavljanxkivcaautclnawcnxxrannslzrzfmzajakbxwvztvikmidgjjyrckisjnrqkivcdzsqxcxjmmjr"
+
+print("Most common: " + charsbycount(teststring)[-1])
+print("10 most common: " + ''.join(charsbycount(teststring)[-10:]))
